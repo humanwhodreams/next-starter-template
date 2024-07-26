789de8d806eb17e2cfdb92e1a0836eb99ebe3cb0
enoabasi <watermelonbit@gmail.com>
2024-07-26 02:34:43 +0100

    refac: update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -16,8 +16,8 @@ function Main() {
   return (
     <section className="flex flex-col items-start flex-1 w-full max-w-4xl mx-auto my-24">
       <Hero />
-      <section className="px-4 mt-8">User stats</section>
-      <section className="px-4">Features</section>
+      {/* <section className="px-4 mt-8">User stats</section>
+      <section className="px-4">Features</section> */}
     </section>
   );
 }
