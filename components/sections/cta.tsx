1812a50edad6d3316669c630beec68d538a73397
enoabasi <watermelonbit@gmail.com>
2024-07-26 01:30:41 +0100

    refac: hero section landing page
    - changed the CTA from a button to a link pointing to the repo

--- components/sections/cta.tsx
+++ components/sections/cta.tsx
@@ -2,8 +2,17 @@ import { FaGithub } from "react-icons/fa";
 
 export function CTA() {
   return (
-    <button className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-border bg-transparent px-6 font-medium text-muted-foreground transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none hover:text-foreground group">
-      <FaGithub size={22} className="mr-2 group-hover:text-foreground text-muted-foreground"/>GitHub
-    </button>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href="https://github.com/fromenoabasi/next-starter-template"
+      className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-border bg-transparent px-6 font-medium text-muted-foreground transition-all [box-shadow:0px_4px_1px_#a3a3a3] active:translate-y-[2px] active:shadow-none hover:text-foreground group"
+    >
+      <FaGithub
+        size={22}
+        className="mr-2 group-hover:text-foreground text-muted-foreground"
+      />
+      GitHub
+    </a>
   );
 }
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -4,7 +4,7 @@ export function Hero() {
   return (
     <section className="px-4 space-y-4 md:px-0">
       <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
-        Kickstart Your Next.js Project with My Starter Template!
+        Kickstart Your Next.js Project with My Starter Template! <span className="text-sky-500">&mdash; It&apos;s Free.</span>
       </h1>
 
       <div className="space-y-2">
