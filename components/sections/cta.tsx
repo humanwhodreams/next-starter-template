826894e9a39d3817c6d516f05f94655a3968a4d9
enoabasi <watermelonbit@gmail.com>
2024-07-26 02:51:37 +0100

    refac: update CTA
    - changed styling of the link and added a link to see internal components

--- components/sections/cta.tsx
+++ components/sections/cta.tsx
@@ -1,18 +1,26 @@
-import { FaGithub } from "react-icons/fa";
+import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
+import { cn } from "@/lib/cn";
 
 export function CTA() {
   return (
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://github.com/fromenoabasi/next-starter-template"
-      className="group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-border bg-background px-6 font-medium text-muted-foreground transition-all [box-shadow:0px_4px_1px_hsl(var(--muted-foreground)/20%)] active:translate-y-[2px] active:shadow-none hover:text-foreground group"
-    >
-      <FaGithub
-        size={22}
-        className="mr-2 group-hover:text-foreground text-muted-foreground"
-      />
-      GitHub
-    </a>
+    <>
+      <a
+        href="https://github.com/fromenoabasi/next-starter-template"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={cn(buttonVariants({ size: "lg" }))}
+      >
+        <ChevronRight className="mr-2 size-4" />
+        See the repository
+      </a>
+      <Link
+        href="components"
+        className="font-medium underline transition-colors duration-200 md:ml-6 decoration-dotted underline-offset-2 hover:text-sky-500"
+      >
+        Browse components
+      </Link>
+    </>
   );
 }
