42e30182992f3c4178f0d79e079f63090fa8c944
enoabasi <watermelonbit@gmail.com>
2024-07-26 02:50:34 +0100

    feat: sign in page
    - added a sign in page
    - added components for form handling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,23 @@
-import * as React from 'react'
+import { ReactNode } from "react";
+import { SiteHeader } from "@/components/shared/site-header";
 
 interface Props {
-    children: React.ReactNode
+  children: ReactNode;
 }
 
 export default function AuthLayout({ children }: Props) {
   return (
-      <main>
-        <header>AuthLayout Header</header>
-        <section>{ children }</section>
-      </main>
-  )
+    <main className="relative flex flex-col items-center justify-between min-h-dvh">
+      <SiteHeader />
+      <Main>{children}</Main>
+    </main>
+  );
+}
+
+function Main({ children }: { children: ReactNode }) {
+  return (
+    <section className="flex flex-col items-start flex-1 w-full max-w-4xl px-4 mx-auto my-24 lg:px-0">
+      {children}
+    </section>
+  );
 }
